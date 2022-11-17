ae89a5183147a0f92cecd3f1fd34c0b7aaab59d9
Felipe Costa <costa.felipe52@gmail.com>
2022-11-17 16:16:47 -0300

    add comentarios

--- client/client.py
+++ client/client.py
@@ -1,13 +1,13 @@
 import socket
 HOST = '127.0.0.1'      # Endereco IP do Servidor
 PORT = 5000             # Porta que o Servidor está
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:   # AF_INET e SOCK_STREAM são constantes
     destino = (HOST, PORT)
-    tcp.connect(destino)
+    tcp.connect(destino)    # Conecta a um socket remoto
     print('Para sair tecle X')
     mensagem = input('--> ')
     while mensagem != 'X':
-        tcp.send(mensagem.encode())
+        tcp.send(mensagem.encode())     # .send Envia a msg e .encode codifica a string
         mensagem = input('--> ')
 print('=== Saindo ===')
-tcp.close()
+tcp.close()     # Fecha a conexão com o socket
--- server/server.py
+++ server/server.py
@@ -1,18 +1,18 @@
 import socket
 HOST = ''               # Endereco IP do Servidor
 PORT = 5000             # Porta que o Servidor está
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     # AF_INET e SOCK_STREAM são constantes
 orig = (HOST, PORT)
 tcp.bind(orig)
-tcp.listen(2)
+tcp.listen(1)           # Habilita o servidor para aceitar conexões
 print('=== Servidor iniciado ===')
 while True:
-    con, cliente = tcp.accept()
+    con, cliente = tcp.accept()     # Aceita uma conexão
     print('*** Conectado por', cliente)
     while True:
-        mensagem = con.recv(1024)
+        mensagem = con.recv(1024)   # Recebe dados do socket
         if not mensagem:
             break
         print(cliente, mensagem)
     print('*** Finalizando conexao do cliente', cliente)
-    con.close()
+    con.close()                     # Fecha a conexão com o socket
